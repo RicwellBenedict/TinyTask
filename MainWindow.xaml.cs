1b8b060bae507e6a6d0a98ad14d1da4c934bb171
Ricwell Fajardo <Fajardo.Ricwell@bene-edu.ch>
2025-11-05 04:53:59 +0100

    Button hinzugefügt i guess

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace TinyTask
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
